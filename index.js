ae5c543fa97769bed867fea4b9074e5a9577cea6
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-10-21 14:30:04 +0700

    feat: create listen & database connection, add .env.example for environment variables database password chore: modifying .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let mysql = require('mysql2');
+require('dotenv').config();
 const app = express();
 const PORT = 3000;
 
@@ -8,4 +9,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'mahasiswa',
+  port: 3309
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:' + err.stack);
+    return;
+  }
+  console.log('Koneksi Berhasil!');
 });
